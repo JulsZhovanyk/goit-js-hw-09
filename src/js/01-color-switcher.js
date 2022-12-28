4a14b619c09ecde1ce7e42924c521aa216af1df4
JulsZhovanyk <julia.zhovanyk@gmail.com>
2022-12-28 23:53:46 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,4 +2,27 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-document.querySelector();
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+let timerSwitch = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+refs.stopBtn.setAttribute('disabled', 'true');
+
+function onStartBtnClick() {
+  refs.startBtn.setAttribute('disabled', 'true');
+  refs.stopBtn.removeAttribute('disabled');
+
+  timerSwitch = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function onStopBtnClick() {
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', 'true');
+  clearInterval(timerSwitch);
+}
